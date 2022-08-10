6479aee07cfdd8b1ac22c360c2e35e2b273c5083
Henrique Vieira <henrique@mvstudio.be>
2022-08-10 16:01:07 +0200

    hero section and stories preview

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 const header = document.querySelector(".header");
 const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".header__nav");
+const body = document.querySelector("body");
 
 window.addEventListener("click", (e) => {
     if (e.target === hamburger) {
         header.classList.toggle("opened");
+        if (header.classList.contains("opened")) {
+            body.classList.add("no-scroll");
+        } else {
+            body.classList.remove("no-scroll");
+        }
     }
 });
